3008efb428d5c5cfa87daccbf9d204de9532832b
Anton3311 <antonplakhotia@gmail.com>
2026-07-03 23:57:49 +0300

    Test with MSVC and Asan

--- build.bat
+++ build.bat
@@ -1,1 +1,2 @@
-clang main.c -o a.exe
+cl /c main.c /Fo:a.obj /DEBUG /Z7 /fsanitize=address
+link a.obj /OUT:a.exe /DEBUG
--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 int main(int argc, char* argv[]) {
 	printf("hello world\n");
 	printf("hello from github actions runner\n");
-	fail
+	int* a = malloc(10);
+	a[14] = 1;
 	return 0;
 }
